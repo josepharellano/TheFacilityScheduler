f9b1ff5410f0ceda0048131565a8c3009e96b697
Xevoxe <xevoxe@gmail.com>
2020-06-06 00:47:17 -0500

    Added Customer Model

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,4 +1,25 @@
 package models;
 
-public class Customer {
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Customer extends  ModelRecord{
+
+    Address address; // Address of the Customer
+    boolean isActive; // Is customer active
+
+    public Customer(int id, String name, Date createDate, String creator, Timestamp lastUpdate,
+                    String lastUpdateBy, Address address,boolean isActive) {
+        super(id, name, createDate, creator, lastUpdate, lastUpdateBy);
+
+        this.address = address;
+        this.isActive = isActive;
+    }
+
+    public Customer(String name,String creator, Address address){
+        super(name,creator);
+
+        this.address = address;
+        this.isActive = true;
+    }
 }
